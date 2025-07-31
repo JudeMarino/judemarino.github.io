45fb42d1995a80afc6d1f9bbbbb59037aebd73bf
JudeMarino <jude.d.marino@gmail.com>
2025-07-30 23:32:25 -0600

    pics

--- WA11/main.js
+++ WA11/main.js
@@ -4,11 +4,7 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
-
 const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
-
-/* Declaring the alternative text for each image file */
 
 const alts = {
     'pic1.jpg': 'lush mountain landscape',
@@ -17,8 +13,6 @@ const alts = {
     'pic4.jpg': 'Abandoned basketball court',
     'pic5.jpg': 'The Flatirons in the fog'
   };
-
-/* Looping through images */
 
 for (const image of images) {
     const newImage = document.createElement('img');
@@ -30,9 +24,6 @@ for (const image of images) {
       displayedImage.alt = e.target.alt;
     });
   }
-
-
-/* Wiring up the Darken/Lighten button */
 
 btn.addEventListener('click', () => {
     const btnClass = btn.getAttribute('class');
